2ad2cb38e62dfcb8d332e6061df85a858bc21322
Santosh <37868195+ssantoshpro@users.noreply.github.com>
2023-06-11 18:00:40 +0530

    Update aws_cli_cmd.sh

--- aws_cli_cmd.sh
+++ aws_cli_cmd.sh
@@ -22,14 +22,14 @@ aws s3 cp "s3://pocbucket06032023/cmd_list.txt" .
 # to run each line 
 cat cmd_list.txt | while read bucket_path; do
 	fname=$(echo $bucket_path | sed -e 's/\r//g')
-	val=$(aws s3 cp s3://"$fname" - | grep -a -o "TRAILER" | head -c -1)
-	if [ "$val" = "TRAILER" ]; then
+	#val=$(aws s3 cp s3://"$fname" - | grep -a -o "TRAILER" | head -c -1)
+	if aws s3 cp s3://"$fname" - | grep -q "TRAILER"; then
 		COUNT=$(aws s3 cp s3://"$fname" - | grep -n 'TRAILER'| cut -d':' -f1 )
 		echo "$fname has $((COUNT-2)) lines"
 		echo "$fname has $((COUNT-2)) lines" >> AWS_Count_Log.txt
-		echo $(aws s3 cp s3://"$fname" - | grep 'TRAILER' ) >> Trailer_Deatil.txt
+		echo "TRAILER-"$(aws s3 cp s3://"$fname" - | grep -m 1 'TRAILER' | sed 's/.*TRAILER//') >> Trailer_Deatil.txt
 	else
-	    COUNT=$(aws s3 cp s3://"$fname" - | wc -l)
+		COUNT=$(aws s3 cp s3://"$fname" - | grep -cve '^\s*$')
 		echo "$fname has $COUNT lines"
 		echo "$fname has $COUNT lines" >> AWS_Count_Log.txt
 	fi
